14a1c3f16f1d4b57300ec6f03b625ddd3fc0d6ca
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-01-10 19:34:11 -0500

    Cleanup & appsettings

--- bot.py
+++ bot.py
@@ -3,27 +3,15 @@ from discord import Embed
 from discord import File
 from discord.ext import commands, tasks
 from dataclasses import dataclass
-import datetime
+import json
 
-# Prefill local values
-BOT_TOKEN = "[Token goes here]"
-CHANNEL_ID = 0
-
-@dataclass
-class Session:
-    is_active: bool = False
-    start_time: int = 0
+# Import keys
+with open('appsettings.local.json') as json:
+    data = json.load(json)
+BOT_TOKEN = data['BotToken']
+CHANNEL_ID = data['HighscoresChannelId']
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-session = Session()
-
-@bot.event
-async def on_ready():
-    print("Hello world")
-
-@bot.command()
-async def test(ctx):
-    await ctx.send("Test")
 
 @bot.command()
 async def updatehighscores(ctx):
