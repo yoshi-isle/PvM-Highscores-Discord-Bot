15ce50133214f06fb3f08e57b27722301e756e45
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-01-14 12:52:27 -0500

    More bosses & raid panels (#18)
    * WIP
    
    * Complete raids embed
    
    o
    
    * Filter top placements

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,7 @@ import json
 import embed
 import database
 from discord.ext import commands
-
+from discord import Embed
 # Import keys
 with open('../config/appsettings.local.json') as settings_json:
     data = json.load(settings_json)
@@ -18,16 +18,54 @@ async def updatehighscores(ctx):
     # Load data
     data = database.GetPersonalBests()
 
-    # Post ToA PBs
     # Post ToB PBs
-    # Post CoX PBs
-    # Post Nightmare PBs
+    await embed.post_raids_embed(ctx, data, "Theatre of Blood", [5, 4, 3, 2, 1], 3)
 
-    # Post DT2 bosses
-    await embed.post_embed(ctx, data, "Vardorvis")
-    await embed.post_embed(ctx, data, "Duke Succellus")
-    await embed.post_embed(ctx, data, "The Whisperer")
-    await embed.post_embed(ctx, data, "Leviathan")
+    # Post HMT PBs
+    await embed.post_raids_embed(ctx, data, "Theatre of Blood: Hard Mode", [5, 4, 3, 2, 1], 3)
+
+    # Post CoX PBs
+    # await embed.post_embed(ctx, data, "Chambers of Xeric", [5, 4, 3, 2, 1], 3)
+
+    # Post CoX: CM PBs
+    # await embed.post_embed(ctx, data, "Chambers of Xeric: Challenge Mode", [5, 3, 2, 1], 3)
+
+    # Post ToA: Expert Mode PBs
+    # await embed.post_embed(ctx, data, "Tombs of Amascut: Expert Mode", [3, 2, 1], 3)
+
+    # Post Nightmare PBs
+    # await embed.post_embed(ctx, data, "Nightmare (Solo)")
+    # await embed.post_embed(ctx, data, "Phosani's Nightmare")
+
+    # Post DT2 PBs
+    await embed.post_boss_embed(ctx, data, "Vardorvis", 3)
+    await embed.post_boss_embed(ctx, data, "Duke Succellus", 3)
+    await embed.post_boss_embed(ctx, data, "The Whisperer", 3)
+    await embed.post_boss_embed(ctx, data, "Leviathan", 3)
+
+    # Post DT2 (Awakened) PBs
+    # await embed.post_embed(ctx, data, "Vardorvis (Awakened)")
+    # await embed.post_embed(ctx, data, "Duke Succellus (Awakened)")
+    # await embed.post_embed(ctx, data, "The Whisperer (Awakened)")
+    # await embed.post_embed(ctx, data, "Leviathan (Awakened)")
+
+    # Post Tzhaar PBs
+    # await embed.post_embed(ctx, data, "Inferno")
+    # await embed.post_embed(ctx, data, "Fight Caves")
+
+    # Post Gauntlet PBs
+    # await embed.post_embed(ctx, data, "The Gauntlet")
+    # await embed.post_embed(ctx, data, "The Corrupted Gauntlet")
+
+    # Post Misc PBs
+    # await embed.post_embed(ctx, data, "Zulrah")
+    # await embed.post_embed(ctx, data, "Vorkath")
+    # await embed.post_embed(ctx, data, "Grotesque Guardians")
+    # await embed.post_embed(ctx, data, "Alchemical Hydra")
+    # await embed.post_embed(ctx, data, "Phantom Muspah")
+    # await embed.post_embed(ctx, data, "Hespori")
+    # await embed.post_embed(ctx, data, "Mimic")
+    # await embed.post_embed(ctx, data, "Hallowed Sepulchre (overall)")
 
 print("Bot running")
 bot.run(bot_token)
--- src/helpers/embed_content_builder.py
+++ src/helpers/embed_content_builder.py
@@ -1,6 +1,6 @@
-def build_embed_content(data, number_of_placements):
+def build_boss_embed_content(data, number_of_placements):
     """
-    Builds formatted embed content from player data, showing top placements. Assumes data is sorted
+    Builds formatted embed content from player data for bosses, showing top placements. Assumes data is sorted
     """
 
     top_placements = data[:number_of_placements] 
@@ -9,8 +9,19 @@ def build_embed_content(data, number_of_placements):
         for place, player in enumerate(top_placements)
     )
 
+def build_raid_embed_content(data, number_of_placements, category):
+  """
+  Builds formatted embed content from player data for raids, showing top placements. Assumes data is sorted
+  """
+  filtered_data = (result for result in data if result["groupSize"] == category)
+  top_placements = list(filtered_data)[:number_of_placements]
+  return "\n".join(
+      f"{placement_emoji[place]} {player['osrsUsername']} - {player['pb']}"
+      for place, player in enumerate(top_placements)
+  )
+
 placement_emoji = {
-    0: ":first_place:",
-    1: ":second_place:",
-    2: ":third_place:",
+  0: ":first_place:",
+  1: ":second_place:",
+  2: ":third_place:",
 }
