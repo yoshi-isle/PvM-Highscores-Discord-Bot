5f735c5968ea103cdc922092637a4a253549122a
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-01-15 12:04:45 -0500

    Remove unneeded update embeds

--- src/embed_generator.py
+++ src/embed_generator.py
@@ -49,55 +49,6 @@ async def post_raids_embed(ctx, data, raid_name, pb_categories, number_of_placem
     await ctx.send(embed=embed)
 
 
-async def update_boss_embed(ctx, data, message, boss_name, number_of_placements):
-    """
-    Updates an embed for boss times
-    """
-    data = helpers.data_helper.get_fastest_times(data, boss_name)
-
-    embed = Embed(title=boss_name)
-
-    embed.set_thumbnail(url=wiki.CDN_URLS[boss_name])
-    embed_content = helpers.embed_content_builder.build_boss_embed_content(
-        data, number_of_placements
-    )
-    embed.add_field(name="", value=embed_content, inline=False)
-
-    # We don't want to rate limit ourselves. Embeds must be posted slowly
-    print("Updating embed for " + boss_name)
-    time.sleep(5)
-    await message.edit(embed=embed)
-
-
-async def update_raids_embed(
-    ctx, data, message, raid_name, pb_categories, number_of_placements
-):
-    """
-    Updates an embed for boss times
-    """
-    data = helpers.data_helper.get_fastest_times(data, raid_name)
-
-    embed = Embed(title=raid_name)
-
-    embed.set_thumbnail(url=wiki.CDN_URLS[raid_name])
-
-    # Iterate through & generate the pb categories we want to show
-    for category in pb_categories:
-        embed_content = helpers.embed_content_builder.build_raid_embed_content(
-            data, number_of_placements, category
-        )
-        if embed_content == "":
-            embed_content = "*(Needs submission)*"
-        embed.add_field(
-            name=category_names[category], value=embed_content, inline=False
-        )
-
-    # We don't want to rate limit ourselves. Embeds must be posted slowly
-    print("Updating embed for " + raid_name)
-    time.sleep(5)
-    await message.edit(embed=embed)
-
-
 category_names = {
     1: "Solo",
     2: "Duo",
