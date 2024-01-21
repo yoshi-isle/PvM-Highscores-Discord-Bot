6244df893c8c8189b5ee175c3c667bd580571bcb
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-01-20 23:54:27 -0500

    Add classes for pb entry and pending submissions

---
+++ src/data/pb_entry.py
@@ -0,0 +1,12 @@
+import uuid
+import time
+
+class PbEntry:
+        id: uuid
+        boss: str
+        pb: time
+        discord_cdn_url: str
+        date_achieved: time
+        osrs_username: str
+        discord_username: str
+      
---
+++ src/data/pending_submission.py
@@ -0,0 +1,12 @@
+import uuid
+import time
+
+class PendingSubmission:
+        id: uuid
+        boss: str
+        pb: time
+        discord_cdn_url: str
+        date_achieved: time
+        osrs_username: str
+        discord_username: str
+      
