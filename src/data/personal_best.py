c9f5e343d8f89b8ab773a0c27c07efed3fd7536e
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-01-22 00:06:58 -0500

    Update personal_best.py

--- src/data/personal_best.py
+++ src/data/personal_best.py
@@ -3,7 +3,7 @@ import time
 from dataclasses import dataclass
 from typing import List
 
-@dataclass(frozen=True)
+@dataclass(frozen=False)
 class PersonalBest:
         id: uuid
         boss: str
@@ -13,21 +13,3 @@ class PersonalBest:
         osrs_username: str
         discord_username: str
         approved: bool
-
-class PersonalBestManager:
-    """
-    Interface to PersonalBest for setting and getting.
-    """
-    def __init__(self):
-        self.pb_list = {}
-
-    def load_pbs(self, pb_list: List[PersonalBest]):
-        for pb_info in pb_list:
-            pb = PersonalBest(**pb_info)
-            self.add_pb(pb)
-
-    def add_pb(self, pb: PersonalBest):
-        self.pb_list[pb.task_number] = pb
-
-    def get_pb(self, boss_name: str, osrs_username: str) -> PersonalBest:
-        return self.tasks.get(boss_name, osrs_username, None)
