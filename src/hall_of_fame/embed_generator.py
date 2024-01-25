ddaeff35c79cb30adb9730ccf57cea24378fdfad
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-01-24 23:59:42 -0500

    Update embed_generator.py

--- src/hall_of_fame/embed_generator.py
+++ src/hall_of_fame/embed_generator.py
@@ -16,39 +16,24 @@ CATERGORY_NAMES = {
 }
 
 
-async def post_boss_embed(ctx, data, boss_info):
+async def post_boss_embed(ctx, data, boss_name, number_of_placements):
     """
     Builds an embed for boss times
     """
-    embed = Embed()
-    embed.set_thumbnail(url=boss_info["thumbnail"])
-
-    fastest_times_data = data = dh.get_fastest_times(data, boss_info["boss_name"])
-    fastest_times_string = await ecb.build_embed_content(fastest_times_data)
-    embed.add_field(
-        name=boss_info["boss_name"], value=fastest_times_string, inline=False
-    )
-
-    await sleep(1)
-    await ctx.send(embed=embed)
-
-    """
     data = dh.get_fastest_times(data, boss_name)
 
-    embed = Embed()
-    embed.set_thumbnail(url=data["thumbnail"])
+    embed = Embed(title=boss_name, color=Colors.light_purple)
 
-    for entry in data["boss_entries"]:
-        # embed_content = await ecb.build_embed_content(data, 3)
-        embed.add_field(name=entry, value="test", inline=False)
+    embed.set_thumbnail(url=wiki.CDN_URLS[boss_name])
+    embed_content = ecb.build_embed_content(data, number_of_placements)
+    embed.add_field(name="", value=embed_content, inline=False)
 
     # We don't want to rate limit ourselves. Embeds must be posted slowly
-    await sleep(1)
+    sleep(1)
     await ctx.send(embed=embed)
-    """
 
 
-async def post_raids_embed(ctx, data, raid_name, pb_categories):
+async def post_raids_embed(ctx, data, raid_name, pb_categories, number_of_placements):
     """
     Builds an embed for raids. Slightly different than bosses because it uses multiple fields
     """
@@ -63,7 +48,7 @@ async def post_raids_embed(ctx, data, raid_name, pb_categories):
             result for result in data if result["groupSize"] == category
         )
 
-        embed_content = await ecb.build_embed_content(filtered_data, 3)
+        embed_content = ecb.build_embed_content(filtered_data, number_of_placements)
         if embed_content == "":
             embed_content = "None"
         embed.add_field(
@@ -73,7 +58,7 @@ async def post_raids_embed(ctx, data, raid_name, pb_categories):
     print("Updating embed for " + raid_name)
 
     # We don't want to rate limit ourselves. Embeds must be posted slowly
-    await sleep(1)
+    sleep(1)
     await ctx.send(embed=embed)
 
 
