39a26648df898ba2fcfc9df68e67164f541865cb
3ear <109140404+3ear@users.noreply.github.com>
2024-01-24 22:38:42 -0700

    turn off file logging and format fixes (#66)

--- src/bot.py
+++ src/bot.py
@@ -56,19 +56,19 @@ async def main():
     root_logger = logging.getLogger("discord")
     root_logger.setLevel(logging.WARNING)
 
-    file_handler = logging.handlers.RotatingFileHandler(
-        filename="discord.log",
-        encoding="utf-8",
-        maxBytes=32 * 1024 * 1024,  # 32 MiB
-        backupCount=5,  # Rotate through 5 files
-    )
+    # file_handler = logging.handlers.RotatingFileHandler(
+    #     filename="discord.log",
+    #     encoding="utf-8",
+    #     maxBytes=32 * 1024 * 1024,  # 32 MiB
+    #     backupCount=5,  # Rotate through 5 files
+    # )
 
     dt_fmt = "%Y-%m-%d %H:%M:%S"
     formatter = logging.Formatter(
         "[{asctime}] [{levelname:<8}] {name}: {message}", dt_fmt, style="{"
     )
-    file_handler.setFormatter(formatter)
-    root_logger.addHandler(file_handler)
+    # file_handler.setFormatter(formatter)
+    # root_logger.addHandler(file_handler)
 
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
