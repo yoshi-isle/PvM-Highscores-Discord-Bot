b033ac992b874e5822131d0a197472c8f9ffd3fa
3ear <109140404+3ear@users.noreply.github.com>
2024-01-28 18:06:32 +0000

    Rever

--- src/constants/channels.py
+++ src/constants/channels.py
@@ -3,10 +3,10 @@ class Guild:
 
 
 class ChannelIds:
-    approve_channel = 1200705952958132274
-    raid_pbs = 1200706301253136484
-    boss_pbs = 1200706301253136484
-    skilling_pbs = 1200706301253136484
-    signup = 1200706362687094845
-    admin_notifications = 1200705967831142430
-    dartboard_commands = 1200705986424483950
+    approve_channel = 1197597093745926175
+    raid_pbs = 1200859576925438053
+    boss_pbs = 1200859569119822044
+    skilling_pbs = 1200858127625629696
+    signup = 1200857007234748507
+    admin_notifications = 1200858495893909555
+    dartboard_commands = 1197597338361925833
