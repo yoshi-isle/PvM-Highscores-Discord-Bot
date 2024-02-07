6ce88bad1aeaf26d840a6a166d5aef4082df4415
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-02-07 18:41:29 -0500

    New forum ids

--- src/constants/channels.py
+++ src/constants/channels.py
@@ -4,13 +4,13 @@ class Guild:
 
 class ChannelIds:
     # IDs for highscores
-    tob_pbs = 1204718096817065994
-    cox_pbs = 1204718254019317800
-    toa_pbs = 1204717942177271879
-    tzhaar_pbs = 1204717280085147648
-    dt2_pbs = 1204717438692761610
-    boss_pbs = 1204717030792626247
-    misc_pbs = 1204716952283779102
+    tob_pbs = 1204934486773604382
+    cox_pbs = 1204934810309623808
+    toa_pbs = 1204934264286609438
+    tzhaar_pbs = 1204933189106409563
+    dt2_pbs = 1204933261839831060
+    boss_pbs = 1204933073729355896
+    misc_pbs = 1204932975947685929
     changelog = 1201225016516300920
 
     # Approve and submission channels
