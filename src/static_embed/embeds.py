1b3b2489ec31d33b093c46849a647e85aa97ecfa
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-02-11 12:08:28 -0500

    Move static embeds

---
+++ src/static_embed/embeds.py
@@ -0,0 +1,86 @@
+import discord
+
+
+def get_candyland_embed():
+    embed = discord.Embed(title="Candyland (2023)")
+
+    embed.add_field(
+        name="",
+        value="<:1stplacecrown:1201249547737894972> Impressed, Alchoholic, Helen Feller. Baked, A Cat Dad, Iron Coosa\n\nhttps://discord.com/channels/847313025919746129/847313574040305704/1097535747902423150",
+        inline=False,
+    )
+
+    embed.set_thumbnail(
+        url="https://oldschool.runescape.wiki/images/thumb/Purple_sweets_detail.png/120px-Purple_sweets_detail.png?41120"
+    )
+    embed.set_footer(text="April 17th, 2023")
+    return embed
+
+
+def get_snakeandlakdders_embed():
+    embed = discord.Embed(title="Snakes & Ladders (2023)")
+
+    embed.add_field(
+        name="",
+        value="<:1stplacecrown:1201249547737894972> Lamhirh, Dopamemes, Adaboy23, Scarlet x3, Zueskin, Rotting\n\nhttps://discord.com/channels/847313025919746129/847313574040305704/1131088765675380847",
+        inline=False,
+    )
+
+    embed.set_thumbnail(
+        url="https://oldschool.runescape.wiki/images/Ladders.png?b5be8&20150310211136"
+    )
+
+    return embed
+
+
+def get_battleofgods_embed():
+    embed = discord.Embed(title="Battle of the Gods (2023)")
+
+    embed.add_field(
+        name="",
+        value="<:1stplacecrown:1201249547737894972> Lilies, Steals, Bir d, justduff, jalals mane, Fat Cat, silly cowboy, Domimic, VirginRabbi, Shypu, Musei, LunasHowl, XtraIcy\n\nhttps://discord.com/channels/847313025919746129/847313574040305704/1148004422153158666",
+        inline=False,
+    )
+
+    embed.set_thumbnail(
+        url="https://oldschool.runescape.wiki/images/thumb/Zamorak%27s_statue_%28Slepe%29.png/122px-Zamorak%27s_statue_%28Slepe%29.png?7e7ee"
+    )
+
+    embed.set_footer(text="September 3rd, 2023")
+    return embed
+
+
+def get_blackcatbingo_embed():
+    embed = discord.Embed(title="Black Cat Halloween Bingo (2023)")
+
+    embed.add_field(
+        name="",
+        value="<:1stplacecrown:1201249547737894972> Shovele, unrot, kitty neko, scarlet x3, Lil Yeeter, Miggy Spoon, Iron Yesu\n\nhttps://discord.com/channels/847313025919746129/847313574040305704/1165755681664667789",
+        inline=False,
+    )
+
+    embed.set_thumbnail(
+        url="https://oldschool.runescape.wiki/images/thumb/Cat_%28black%29.png/180px-Cat_%28black%29.png?1dfde"
+    )
+
+    embed.set_footer(text="October 22nd, 2023")
+
+    return embed
+
+
+def get_dartboard_embed():
+    embed = discord.Embed(title="Zueskin's Dartboard (2024)")
+
+    embed.add_field(
+        name="",
+        value="<:1stplacecrown:1201249547737894972> gee i wonder whos gonna win this one better leave it blank just in case",
+        inline=False,
+    )
+
+    embed.set_footer(text="February 11th, 2024")
+
+    embed.set_thumbnail(
+        url="https://oldschool.runescape.wiki/images/thumb/Target_%28Ranging_Guild%29.png/150px-Target_%28Ranging_Guild%29.png?89891"
+    )
+
+    return embed
--- src/static_embed/static_embed_cog.py
+++ src/static_embed/static_embed_cog.py
@@ -1,24 +1,18 @@
 import logging
-from typing import Literal, Optional
-
 import discord
-from discord import app_commands
 from discord.ext import commands
-from management.random_emoji import (
-    get_random_achievement_emoji,
-    get_random_drop_emoji,
-    get_random_floof_emoji,
-)
-from management.random_greeting import get_random_greeting_url
-
-from constants.channels import ChannelIds
 from math import ceil
+import static_embed.embeds as embeds
 
 
 class StaticEmbed(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
         self.logger = logging.getLogger("discord")
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.logger.info("static embed cog loaded")
 
     @commands.command()
     @commands.guild_only()
@@ -55,86 +49,11 @@ class StaticEmbed(commands.Cog):
             )
             await ctx.send(embed=embed)
 
-        embed.set_thumbnail(
-            url="https://oldschool.runescape.wiki/images/Stats_icon.png?1b467&20160515204513"
-        )
-
-        embed = discord.Embed(title="Candyland (2023)")
-
-        embed.add_field(
-            name="",
-            value="<:1stplacecrown:1201249547737894972> Impressed, Alchoholic, Helen Feller. Baked, A Cat Dad, Iron Coosa\n\nhttps://discord.com/channels/847313025919746129/847313574040305704/1097535747902423150",
-            inline=False,
-        )
-
-        embed.set_thumbnail(
-            url="https://oldschool.runescape.wiki/images/thumb/Purple_sweets_detail.png/120px-Purple_sweets_detail.png?41120"
-        )
-        embed.set_footer(text="April 17th, 2023")
-
-        await ctx.send(embed=embed)
-
-        embed = discord.Embed(title="Snakes & Ladders (2023)")
-
-        embed.add_field(
-            name="",
-            value="<:1stplacecrown:1201249547737894972> Lamhirh, Dopamemes, Adaboy23, Scarlet x3, Zueskin, Rotting\n\nhttps://discord.com/channels/847313025919746129/847313574040305704/1131088765675380847",
-            inline=False,
-        )
-
-        embed.set_thumbnail(
-            url="https://oldschool.runescape.wiki/images/Ladders.png?b5be8&20150310211136"
-        )
-
-        await ctx.send(embed=embed)
-
-        embed = discord.Embed(title="Battle of the Gods (2023)")
-
-        embed.add_field(
-            name="",
-            value="<:1stplacecrown:1201249547737894972> Lilies, Steals, Bir d, justduff, jalals mane, Fat Cat, silly cowboy, Domimic, VirginRabbi, Shypu, Musei, LunasHowl, XtraIcy\n\nhttps://discord.com/channels/847313025919746129/847313574040305704/1148004422153158666",
-            inline=False,
-        )
-
-        embed.set_thumbnail(
-            url="https://oldschool.runescape.wiki/images/thumb/Zamorak%27s_statue_%28Slepe%29.png/122px-Zamorak%27s_statue_%28Slepe%29.png?7e7ee"
-        )
-
-        embed.set_footer(text="September 3rd, 2023")
-
-        await ctx.send(embed=embed)
-
-        embed = discord.Embed(title="Black Cat Halloween Bingo (2023)")
-
-        embed.add_field(
-            name="",
-            value="<:1stplacecrown:1201249547737894972> Shovele, unrot, kitty neko, scarlet x3, Lil Yeeter, Miggy Spoon, Iron Yesu\n\nhttps://discord.com/channels/847313025919746129/847313574040305704/1165755681664667789",
-            inline=False,
-        )
-
-        embed.set_thumbnail(
-            url="https://oldschool.runescape.wiki/images/thumb/Cat_%28black%29.png/180px-Cat_%28black%29.png?1dfde"
-        )
-
-        embed.set_footer(text="October 22nd, 2023")
-
-        await ctx.send(embed=embed)
-
-        embed = discord.Embed(title="Zueskin's Dartboard (2024)")
-
-        embed.add_field(
-            name="",
-            value="<:1stplacecrown:1201249547737894972> gee i wonder whos gonna win this one better leave it blank just in case",
-            inline=False,
-        )
-
-        embed.set_footer(text="February 11th, 2024")
-
-        embed.set_thumbnail(
-            url="https://oldschool.runescape.wiki/images/thumb/Target_%28Ranging_Guild%29.png/150px-Target_%28Ranging_Guild%29.png?89891"
-        )
-
-        await ctx.send(embed=embed)
+        await ctx.send(embed=embeds.get_candyland_embed())
+        await ctx.send(embed=embeds.get_snakeandlakdders_embed())
+        await ctx.send(embed=embeds.get_battleofgods_embed())
+        await ctx.send(embed=embeds.get_blackcatbingo_embed())
+        await ctx.send(embed=embeds.get_dartboard_embed())
 
 
 async def setup(bot):
