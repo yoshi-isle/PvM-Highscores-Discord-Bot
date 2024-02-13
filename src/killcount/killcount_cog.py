2a0789aa8c9ee955e54bbbbd2a9198b57cec12cc
3ear <paul@zuskind.com>
2024-02-13 06:40:05 -0700

    move sleep to outside individual boss loops

--- src/killcount/killcount_cog.py
+++ src/killcount/killcount_cog.py
@@ -68,8 +68,7 @@ class KillCount(commands.Cog):
             iron = IRON_ICON + f" {irons[0].player.display_name} - **{iron_kc} KC**\n"
 
             embed.add_field(name=f" __{boss_name}__", value=normie + "\n" + iron, inline=False)
-            await sleep(15) # take it easy on the wom api rates
-
+        await sleep(15) # take it easy on the wom api rates
         return embed
 
 
