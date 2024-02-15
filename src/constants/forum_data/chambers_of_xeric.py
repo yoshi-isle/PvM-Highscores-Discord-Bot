28f44d74429993b6845b2a029871f7a23db7a0b1
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-02-15 12:40:58 -0500

    na

--- src/constants/forum_data/chambers_of_xeric.py
+++ src/constants/forum_data/chambers_of_xeric.py
@@ -21,10 +21,6 @@ INFO = [
     },
     {
         "thumbnail": "https://oldschool.runescape.wiki/images/thumb/Metamorphic_dust_detail.png/150px-Metamorphic_dust_detail.png?d0e85",
-        "boss_name": "Chambers of Xeric: Challenge Mode (4 man)",
-    },
-    {
-        "thumbnail": "https://oldschool.runescape.wiki/images/thumb/Metamorphic_dust_detail.png/150px-Metamorphic_dust_detail.png?d0e85",
         "boss_name": "Chambers of Xeric: Challenge Mode (Trio)",
     },
     {
