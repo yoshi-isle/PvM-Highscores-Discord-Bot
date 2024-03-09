5e52a9263822892000e1187521c2c3ccdc60d888
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-03-09 00:29:35 -0500

    Add static stuff (#97)
    * Add static stuff
    
    * Space
    
    * Playernames class
    
    * Fixge
    
    * fun stats
    
    * Smol fixes
    
    * Fin

--- src/static_embed/embeds.py
+++ src/static_embed/embeds.py
@@ -1,4 +1,5 @@
 import discord
+from static_embed.constants.player_names import PlayerNames 
 
 NORMIE_ICON = "<:main:1206053914873565266>"
 IRON_ICON = "<:ironman:1207739589784113182>"
@@ -6,19 +7,28 @@ COOKING_ICON = "<:cooking:1206108579480862730>"
 THIEVING_ICON = "<:thieving:1206108085056442378>"
 CROWN_ICON = "<:1stplacecrown:1201249547737894972>"
 SECONDPLACECROWN_ICON = "<:2ndplacecrown:1201249561423917248>"
+THIRDPLACECROWN_ICON = "<:3rdplacecrown:1201249572664643664>"
 FARMING_ICON = "<:farming:1206108755499024424>"
 
 
 def get_candyland_embed():
     embed = discord.Embed(title="Candyland (2023)")
-
+    
     embed.add_field(
-        name="",
-        value=f"{CROWN_ICON} Impressed, Alchoholic, Helen Feller. Baked, A Cat Dad, Iron Coosa\n\nhttps://discord.com/channels/847313025919746129/847313574040305704/1097535747902423150",
-        inline=False,
-    )
-
+            name="",
+            value=", ".join(
+                [
+                CROWN_ICON + ' ' +  PlayerNames.impressed,
+                PlayerNames.kirby,
+                PlayerNames.helen_feller,
+                PlayerNames.baked,
+                PlayerNames.cat_dad,
+                PlayerNames.iron_coosa]),
+            inline=False,
+        )
+    
     embed.set_thumbnail(url="https://oldschool.runescape.wiki/images/thumb/Purple_sweets_detail.png/120px-Purple_sweets_detail.png?41120")
+
     embed.set_footer(text="April 17th, 2023")
     return embed
 
@@ -27,10 +37,17 @@ def get_snakeandladders_embed():
     embed = discord.Embed(title="Snakes & Ladders (2023)")
 
     embed.add_field(
-        name="",
-        value=f"{CROWN_ICON} Lamhirh, Dopamemes, Adaboy23, scarlet x3, Zueskin, Rotting\n\nhttps://discord.com/channels/847313025919746129/847313574040305704/1131088765675380847",
-        inline=False,
-    )
+            name="",
+            value=", ".join(
+                [
+                CROWN_ICON + ' ' +  PlayerNames.lamhirh,
+                PlayerNames.dopamemes,
+                PlayerNames.adaboy,
+                PlayerNames.scarlet_cat,
+                PlayerNames.zueskin,
+                PlayerNames.rotting]),
+            inline=False,
+        )
 
     embed.set_thumbnail(url="https://oldschool.runescape.wiki/images/Ladders.png?b5be8&20150310211136")
 
@@ -42,10 +59,24 @@ def get_battleofgods_embed():
     embed = discord.Embed(title="Battle of the Gods (2023)")
 
     embed.add_field(
-        name="",
-        value=f"{CROWN_ICON} Lilies, Steals, Bir d, justduff, jalals mane, Fat Cat, silly cowboy, Domimic, VirginRabbi, Shypu, Musei, LunasHowl, XtraIcy\n\nhttps://discord.com/channels/847313025919746129/847313574040305704/1148004422153158666",
-        inline=False,
-    )
+            name="",
+            value=", ".join(
+                [
+                CROWN_ICON + ' ' +  PlayerNames.lilies,
+                PlayerNames.steals,
+                PlayerNames.bird,
+                PlayerNames.justduff,
+                PlayerNames.jalal_mane,
+                PlayerNames.fat_cat,
+                PlayerNames.silly_cowboy,
+                PlayerNames.domimic,
+                PlayerNames.virgin_rabbit,
+                PlayerNames.shypu,
+                PlayerNames.musei,
+                PlayerNames.lunas_howl,
+                PlayerNames.xtra_icy,]),
+            inline=False,
+        )
 
     embed.set_thumbnail(url="https://oldschool.runescape.wiki/images/thumb/Zamorak%27s_statue_%28Slepe%29.png/122px-Zamorak%27s_statue_%28Slepe%29.png?7e7ee")
 
@@ -57,10 +88,18 @@ def get_blackcatbingo_embed():
     embed = discord.Embed(title="Black Cat Halloween Bingo (2023)")
 
     embed.add_field(
-        name="",
-        value=f"{CROWN_ICON} nora cat, unrot, cat nya, scarlet x3, Lil Yeeter, Miggy Spoon, Iron Yesu\n\nhttps://discord.com/channels/847313025919746129/847313574040305704/1165755681664667789",
-        inline=False,
-    )
+            name="",
+            value=", ".join(
+                [
+                CROWN_ICON + ' ' +  PlayerNames.nora_cat,
+                PlayerNames.unrot,
+                PlayerNames.neko,
+                PlayerNames.scarlet_cat,
+                PlayerNames.lil_yeeter,
+                PlayerNames.miggy_spoon,
+                PlayerNames.iron_yesu,]),
+            inline=False,
+        )
 
     embed.set_thumbnail(url="https://oldschool.runescape.wiki/images/thumb/Cat_%28black%29.png/180px-Cat_%28black%29.png?1dfde")
 
@@ -73,10 +112,21 @@ def get_dartboard_embed():
     embed = discord.Embed(title="Zueskin's Dartboard")
 
     embed.add_field(
-        name="",
-        value=f"{CROWN_ICON} unrot, nora cat, Rat K ing, Viables, GIM unrot, Lt Kasper, PAWtrol, Bornfury95, Wrldsbstsimp, The Norden\n\nhttps://discord.com/channels/847313025919746129/847313574040305704/1206795998077132810",
-        inline=False,
-    )
+            name="",
+            value=", ".join(
+                [
+                CROWN_ICON + ' ' +  PlayerNames.unrot,
+                PlayerNames.nora_cat,
+                PlayerNames.rat_king,
+                PlayerNames.viables,
+                PlayerNames.gerg,
+                PlayerNames.lt_kasper,
+                PlayerNames.pawtrol,
+                PlayerNames.bornfury95,
+                PlayerNames.wrldsbestsmp,
+                PlayerNames.norden,]),
+            inline=False,
+        )
 
     embed.set_footer(text="February 11th, 2024")
 
@@ -89,10 +139,16 @@ def get_grandmasters_embed():
     embed = discord.Embed(title="__Grandmaster CA's__")
 
     embed.add_field(
-        name="",
-        value="G Fredo\nYoshe\nViables\netyl\nsolstice\nunrot",
-        inline=False,
-    )
+            name="",
+            value="\n".join(
+                PlayerNames.g_fredo,
+                PlayerNames.yoshe,
+                PlayerNames.viables,
+                PlayerNames.etyl,
+                PlayerNames.solstice,
+                PlayerNames.unrot),
+            inline=False,
+        )
 
     embed.set_thumbnail(url="https://oldschool.runescape.wiki/images/Tzkal_slayer_helmet_chathead.png?ee6a6")
     return embed
@@ -103,11 +159,24 @@ def get_fun_stats():
         title="__Blood Torva Gang__",
     )
 
+
     embed.add_field(
-        name="",
-        value=f"Yoshe\nDopamemes\nEtyl\nCrayy\nunrot\nNot Solstice\nRat K ing\nG Fredo\nCentac\nViables\nmy name gerg",
-        inline=False,
-    )
+            name="",
+            value="\n".join(
+                PlayerNames.yoshe,
+                PlayerNames.dopamemes,
+                PlayerNames.etyl,
+                PlayerNames.crayy,
+                PlayerNames.unrot,
+                PlayerNames.solstice,
+                PlayerNames.rat_king,
+                PlayerNames.g_fredo,
+                PlayerNames.centac,
+                PlayerNames.viables,
+                PlayerNames.gerg,
+                PlayerNames.cats_go_nya),
+            inline=False,
+        )
 
     embed.set_thumbnail(
         url="https://oldschool.runescape.wiki/images/thumb/Ancient_blood_ornament_kit_detail.png/130px-Ancient_blood_ornament_kit_detail.png?4b3c1"
@@ -123,7 +192,7 @@ def get_clogs():
 
     embed.add_field(
         name="",
-        value=f"{NORMIE_ICON} Etyl - 1,000\n{IRON_ICON} Kainsaw - 868",
+        value=f"{NORMIE_ICON} {PlayerNames.etyl} - 1,000+\n{IRON_ICON} {PlayerNames.kainsaw} - 868+",
         inline=False,
     )
 
@@ -139,7 +208,7 @@ def get_200ms():
 
     embed.add_field(
         name="",
-        value=f"Devvy uwu {THIEVING_ICON}\nKainsaw {COOKING_ICON}\nMusei {FARMING_ICON}",
+        value=f"{PlayerNames.devvy_uwu} {THIEVING_ICON}\n{PlayerNames.kainsaw} {COOKING_ICON}\n{PlayerNames.musei} {FARMING_ICON}",
         inline=False,
     )
 
@@ -155,7 +224,7 @@ def get_tears():
 
     embed.add_field(
         name="",
-        value=f"{CROWN_ICON} Crayy - 240 Tears\n{SECONDPLACECROWN_ICON}Adaboy23 - 226 Tears",
+        value=f"{CROWN_ICON} {PlayerNames.crayy} - 240 Tears\n{SECONDPLACECROWN_ICON} {PlayerNames.adaboy} - 238 Tears\n{THIRDPLACECROWN_ICON} {PlayerNames.imbes} - 231 Tears",
         inline=False,
     )
 
