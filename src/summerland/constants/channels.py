c470bfd7ccbc6e59832659b58ade4527eda81223
yoshi-isle <yoshi-isle>
2024-05-31 20:32:14 -0400

    ready to start event yay

--- src/summerland/constants/channels.py
+++ src/summerland/constants/channels.py
@@ -1,5 +1,5 @@
 class ChannelIds:
-    current_standings = 1237804690570481715
-    approve_channel = 1241965921032081428
+    current_standings = 1245222266225168424
+    approve_channel = 1246112185294262315
     admin_notifications = 1241506750306189383
-    changelog = 1245371792248537169
+    changelog = 1246111728622633132
