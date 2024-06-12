3337c723b8114169f9cb2340fe8f198303b8eebc
yoshi-isle <yoshi-isle>
2024-06-12 01:21:11 -0400

    there

--- src/summerland/embed_generator.py
+++ src/summerland/embed_generator.py
@@ -34,6 +34,7 @@ async def generate_final_results():
         description=":1stplacecrown: <:orca:1245396728094654485> Orca Team\n:2ndplacecrown: <:octopus:1245396876426215574> Squidrific\n\nWhalers <:whale:1246256962815918183> - Tile 100: 5x Echo Crystals OR 1x Ralos (MUST BE COMPLETED)\nThe Busty Crustaceans <:crab:1245396597345615983> - Tile 96: Any 3x Lightbearers or Fangs\nNemo <:clownfish:1245396742065881241> - Tile 91: 5x Armor Seeds OR Enhanced Weapon Seed\nRays <:stingray:1246257550559547442> - Tile 88: Any Zalcano unique\nOnlyPhins <:dolphin:1245396627213258833> - Tile 80: Feather Hunter CA\nSea Turtle <:sea_turtle:1245396703977406605> - Tile 58: Any NM unique (not Slepey Tablet)",
         colour=Colors.green,
     )
+    trailblazer_trophy_image_url = "https://oldschool.runescape.wiki/images/Trailblazer_reloaded_dragon_trophy.png?4f4fe"
     embed.set_thumbnail(url=trailblazer_trophy_image_url)
     embed.set_footer(
         text="discord.gg/kittycats", icon_url="https://i.imgur.com/RT1AlJj.png"
