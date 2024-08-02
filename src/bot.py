671914711512324727dc6c88ccedc38574698ab3
yoshi-isle <123754082+yoshi-isle@users.noreply.github.com>
2024-08-02 19:20:37 -0400

    Disable summer cog

--- src/bot.py
+++ src/bot.py
@@ -108,7 +108,7 @@ async def main():
         "static_embed.static_embed_cog",
         "killcount.killcount_cog",
         "hall_of_fame.hall_of_fame_cog",
-        "summerland.summerland_cog",
+        # "summerland.summerland_cog",
     ]
 
     async with CustomBot(
